3d06186f7b038c52d8fabe78953dcd50250b1a61
comp <evgekha.krutoy@gmail.com>
2023-04-03 19:16:57 +0300

    Нужно добавить анимации

--- ООП/main.py
+++ ООП/main.py
@@ -6,38 +6,66 @@ from pygame_widgets.button import Button
 # Set up Pygame
 pygame.init()
 screen = pygame.display.set_mode((320, 320))
-turn = 1
-table = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+table = [['o', 'o', 'o'], ['o', 'o', 'o'], ['o', 'o', 'o']]
 
 
 class Table:
     __r = 0
     __c = 0
-
+    turn = 'a'
     def __init__(self):
+        self.alraedy_clicked = False
         x = int(Table.__c * 110)
         y = int(Table.__r * 110)
-        self.btn = Button(screen, x, y, 100, 100, onClick=lambda: set_figure(int(y / 110), int(x / 110)))
+        self.btn = Button(screen, x, y, 100, 100, onClick=lambda: self.set_figure(int(y / 110), int(x / 110)))
         if Table.__c <= 2:
             Table.__c += 1
         else:
             Table.__c = 0
             Table.__r += 1
 
+    def set_figure(self, r, c):
+        if self.alraedy_clicked:
+            print('Already')
+            pass
+        else:
+            if Table.turn == 'a':
+                table[r][c] = 'a'
+                Table.turn = 'b'
+            elif Table.turn == 'b':
+                table[r][c] = 'b'
+                Table.turn = 'a'
+            self.alraedy_clicked = True
+            print(table)
+    @staticmethod
+    def is_win():
+        global run
+        if ''.join(table[0]) == 'aaa' or ''.join(table[1]) == 'aaa' or ''.join(table[2]) == 'aaa':
+            print('Крестики победили')
+            run = False
+        if (table[0][0] == 'a' and table[1][0] == 'a' and table[2][0] == 'a') or (table[0][1] == 'a' and table[1][1] == 'a' and table[2][1] == 'a') or (table[0][2] == 'a' and table[1][2] == 'a' and table[2][2] == 'a'):
+            print('Крестики победили')
+            run = False
+        if (table[0][0] == 'a' and table[1][1] == 'a' and table[2][2] == 'a') or (table[0][2] == 'a' and table[1][1] == 'a' and table[2][0] == 'a'):
+            print('Крестики победили')
+            run = False
 
-def set_figure(r, c):
-    global turn
-    if turn == 1:
-        table[r][c] = 1
-        turn = 2
-    elif turn == 2:
-        table[r][c] = 2
-        turn = 1
-    pygame.draw.circle(screen, (111, 20, 222), (200, 100), 50, 10)
-    print(table)
+        if ''.join(table[0]) == 'bbb' or ''.join(table[1]) == 'bbb' or ''.join(table[2]) == 'bbb':
+            print('Нолики победили')
+            run = False
+        if (table[0][0] == 'b' and table[1][0] == 'b' and table[2][0] == 'b') or (table[0][1] == 'b' and table[1][1] == 'b' and table[2][1] == 'b') or (table[0][2] == 'b' and table[1][2] == 'b' and table[2][2] == 'b'):
+            print('Нолики победили')
+            run = False
+        if (table[0][0] == 'b' and table[1][1] == 'b' and table[2][2] == 'b') or (table[0][2] == 'b' and table[1][1] == 'b' and table[2][0] == 'b'):
+            print('Нолики победили')
+            run = False
+
 
 
 objs = [Table() for i in range(11)]
+
+
 
 run = True
 while run:
@@ -49,6 +77,7 @@ while run:
             quit()
 
     screen.fill((92, 92, 94))
-
+    Table.is_win()
     pygame_widgets.update(events)
     pygame.display.update()
+
